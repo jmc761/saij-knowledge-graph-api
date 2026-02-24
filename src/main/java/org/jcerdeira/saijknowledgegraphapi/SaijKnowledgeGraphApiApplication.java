2c6e96cd9a10fc7689e7f43e27b769c47fa488a9
Juan Manuel Cerdeira <juanmanuelcerdeira761@gmail.com>
2026-02-24 13:37:18 -0300

    Initial commit

---
+++ src/main/java/org/jcerdeira/saijknowledgegraphapi/SaijKnowledgeGraphApiApplication.java
@@ -0,0 +1,13 @@
+package org.jcerdeira.saijknowledgegraphapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaijKnowledgeGraphApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaijKnowledgeGraphApiApplication.class, args);
+	}
+
+}
